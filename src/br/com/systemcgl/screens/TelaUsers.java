e356765781f463f456b3d0d829de20fbad287906
systemcgl <systemcgl@gmail.com>
2014-06-30 20:05:14 -0300

    Update TelaUsers.java

--- src/br/com/systemcgl/screens/TelaUsers.java
+++ src/br/com/systemcgl/screens/TelaUsers.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import net.proteanit.sql.DbUtils;
 import br.com.systemcgl.Fachada;
 import br.com.systemcgl.entidades.Usuario;
